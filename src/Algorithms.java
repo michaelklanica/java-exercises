ed16720b1ac4513afb53da114aae6cb18c9a0594
Michael Klanica <michael.david.klanica@gmail.com>
2021-04-14 00:14:59 -0500

    Exercise 2 complete

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -4,11 +4,16 @@ public class Algorithms {
 
     public static void main(String[] args) {
 
+//        EXERCISE 1:
         System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 4}, 3)));
+
+//        EXERCISE 2:
+        System.out.println(reverseInt(1234567890));
+
 
     }
 
-//    EXERCISE 1:
+//    EXERCISE 1 (APAS 1):
 //    GIVEN AN ARRAY OF INTEGERS, RETURN INDICES OF THE TWO NUMBERS SUCH THAT THEY ADD UP TO A SPECIFIC TARGET.
 //    YOU MAY ASSUME THAT EACH INPUT WILL HAVE EXACTLY ONE SOLUTION, AND YOU MAY NOT USE THE SAME ELEMENT TWICE.
 
@@ -23,6 +28,21 @@ public class Algorithms {
         throw new IllegalArgumentException("No two sum solution");
     }
 
+//    EXERCISE 2 (APAS 7):
+//    GIVEN A 32-BIT SIGNED INTEGER, RETURN THE REVERSE DIGITS OF THE INTEGER.
+//    123 => 321 ; -123 => -321 ; 120 => 21
+//    ASSUME YOU ARE DEALING WITH AN ENVIRONMENT WHICH CAN ONLY STORE INTEGERS WITHIN THE 32-BIT SIGNED INTEGER RANGE
+//    (-231 - 230). ASSUME THE FUNCTION RETURNS 0 WHEN THE REVERSED INTEGER OVERFLOWS.
+
+    public static int reverseInt(int inNum) {
+        long outNum = 0;
+        while (inNum != 0) {
+            outNum = outNum * 10 + inNum % 10;
+            inNum = inNum / 10;
+        }
+        if (outNum > Integer.MAX_VALUE || outNum < Integer.MIN_VALUE) return 0;
+        return (int) outNum;
+    }
 
 
 }
